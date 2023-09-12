0177166d8aee0aca4000ca5fe3fa9446517188a8
谷下雨 <1197501385@qq.com>
2023-09-12 14:47:19 +0800

    Delete Modu directory

--- Modu/data/__init__.py
+++
@@ -1,3 +0,0 @@
-from .ge2e_dataset import GE2EDataset, collate_batch
-from .wav2mel import Wav2Mel
-from .infinite_dataloader import InfiniteDataLoader, infinite_iterator
--- Modu/modules/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dvector import *
-from .ge2e import GE2ELoss
